655376a933022d120cade83a4300ef44fe5af9dc
dead-horse <dead_horse@qq.com>
2016-09-02 14:56:10 +0800

    test: create table

--- test/prepare.js
+++ test/prepare.js
@@ -2,5 +2,6 @@
 
 const execSync = require('child_process').execSync;
 
+execSync('mysql -uroot -e "create database IF NOT EXISTS test;"');
 execSync('mysql -uroot test < test/npm_auth.sql');
 console.log('create table success');
